672cf1fb0510fbcc1a1ec844ce7c7611a8ebfb7e
ryoua <1794970183@qq.com>
2020-11-13 21:47:42 +0800

    新增阿里云OSS上传文件

---
+++ muse-core/src/main/java/com/muse/common/config/AliyunOSSConfigConstant.java
@@ -0,0 +1,22 @@
+package com.muse.common.config;
+
+/**
+ * * @Author: RyouA
+ * * @Date: 2020/11/13
+ **/
+public class AliyunOSSConfigConstant {
+    private AliyunOSSConfigConstant() {
+    }
+
+    public static final String URL_PREFIX = "https://ryoua.oss-cn-shanghai.aliyuncs.com/";
+
+    public static final String BUCKET_NAME = "ryoua";
+
+    public static final String END_POINT = "oss-cn-shanghai.aliyuncs.com";
+    // AccessKey ID
+    public static final String AccessKey_ID = "LTAI4FeJduE45dWT7B4xfEsf";
+    // Access Key Secret
+    public static final String AccessKey_Secret = "AKd9r6CkOBAbPk2h8aSy9kK9mH6A3x";
+    // 仓库中的某个文件夹
+    public static final String FILE_HOST = "muse";
+}
--- muse-core/src/main/java/com/muse/common/controller/CommonController.java
+++ muse-core/src/main/java/com/muse/common/controller/CommonController.java
@@ -1,22 +1,35 @@
 package com.muse.common.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.muse.common.exception.MuseException;
+import com.muse.common.util.AliyunOSSUtil;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 
 
 /**
  * * @Author: RyouA
  * * @Date: 2020/10/26
  **/
-@Controller
+@RestController
 public class CommonController {
-    @GetMapping(value="/**")
-    public String index(){
-        return "index";
+    private static final String UTIL_TAG = "工具类";
+
+
+    @PostMapping("/upload")
+    @ApiOperation(value = "上传文件", tags = UTIL_TAG, httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "file", value = "文件", required = true, dataType = "file"),
+    })
+    public String upload(@RequestParam MultipartFile file) {
+         return AliyunOSSUtil.upLoad(file);
     }
 }
